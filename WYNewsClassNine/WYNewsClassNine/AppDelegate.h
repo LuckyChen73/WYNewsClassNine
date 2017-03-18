1d35a18a754b28ff853671d7d0cf6e62345cc687
CHENWEI73 <2102657566@qq.com>
2017-03-18 19:13:47 +0800

    1。项目准备完成

---
+++ WYNewsClassNine/WYNewsClassNine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYNewsClassNine
+//
+//  Created by chenWei on 2017/3/18.
+//  Copyright © 2017年 陈伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
